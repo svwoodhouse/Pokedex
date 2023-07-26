156beaf5ffa2f6c48c3e46590c5bc2cf78bc91f8
Sydnee <svwoodhouse@gmail.com>
2023-07-25 22:14:12 -0400

    Added connection for MongoDB

--- server/index.js
+++ server/index.js
@@ -1,10 +1,22 @@
 // server/index.js
-
 const express = require("express");
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const PORT = process.env.PORT || 3001;
+const MONGOURL = process.env.MONGOURL
 
 const app = express();
+mongoose.connect(MONGOURL);
+const database = mongoose.connection
+
+database.on('error', (error) => {
+    console.log(error)
+})
+
+database.once('connected', () => {
+    console.log('Database Connected');
+})
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
